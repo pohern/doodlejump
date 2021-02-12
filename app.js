538da30d05742c63df613e778c95def0bda4cb1b
pohern <ohern_p@yahoo.com>
2021-02-12 11:28:32 -0600

    add platforms to page

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
+    let isGameOver = false
+    let platformCount = 5
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -10,5 +12,35 @@ document.addEventListener('DOMContentLoaded', () => {
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
-    createDoodler()
+    
+    class Platform {
+        constructor(newPlatformBottom) {
+            this.bottom = newPlatformBottom 
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms(){
+        for (let i=0; i < platformCount; i++){
+            let platformGap = 600 / platformCount
+            let newPlatfromBottom = 100 + i * platformGap
+            let newPlatform = new Platform(newPlatfromBottom)
+        }
+    }
+
+    function start() {
+        if (!isGameOver){
+            createDoodler();
+            createPlatforms()
+        }
+    }
+    //Attach to a button
+    start()
 })
